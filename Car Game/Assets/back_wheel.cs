06975e24fcc878240492efda4f77f4c5d11d5c28
(전자공학부)송상현 <kr2519@kookmin.kr>
2019-10-14 00:30:27 +0900

    floor

---
+++ Car Game/Assets/back_wheel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class back_wheel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTRiggerEnter");
+        if (other.gameObject.CompareTag("TargetBall"))
+        {
+            other.gameObject.SetActive(false);
+            //other.gameObject.GetComponent<BallScript>().DoInitPos();
+            Debug.Log("TargetBall false");
+        }
+    }
+}
